7fb08081f20dc189a6d2c3b177b847b8ff25b6a5
shaeel45 <beingshaeel@gmail.com>
2023-12-09 23:08:58 +0500

    Working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,OnInit} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { Title } from '@angular/platform-browser';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", component:HomeComponent, data:{title:"Home"}},
+  { path: "about", component: AboutComponent, data: { title: "About" }  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule implements OnInit{
+
+constructor(private titleService:Title){
+  this.titleService.setTitle("Home")
+ 
+}
+
+ngOnInit(): void {
+    
+  }
+;
+
+
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
